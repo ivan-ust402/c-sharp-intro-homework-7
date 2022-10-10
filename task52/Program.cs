321e8a42f87a7d491f1ce63f184a40fee34af9d3
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-10 19:50:00 +0700

    Created FillArray and PrintArray function

---
+++ task52/Program.cs
@@ -0,0 +1,44 @@
+﻿/* 
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+Console.Clear();
+Console.WriteLine("Введите желаемую размерность массива.");
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+
+
+void FillArray(int[,] array)
+{
+    Random randomGen = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = randomGen.Next(-10, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
