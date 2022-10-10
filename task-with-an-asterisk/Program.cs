83b770921a4a70fd5e5f03aa5690354c8f9f6363
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-10 21:09:46 +0700

    Completed task with an asterisk

---
+++ task-with-an-asterisk/Program.cs
@@ -0,0 +1,37 @@
+﻿/* 
+Задача *. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+*/
+
+Console.Clear();
+Console.Write("Введите число для подсчета фибоначчи: ");
+int number = int.Parse(Console.ReadLine()!);
+
+double[] array = new double[number]; 
+double[] fib = Fibonacci(number, array);
+PrintArray(fib);
+
+double[] Fibonacci(int number, double[] array)
+{
+    for (int i = 0; i < number; i++) 
+    {
+        if (i == 0) array[0] = 0;
+        else if (i == 1) array[1] = 1;
+        else {
+            array[i] = array[i-1] + array[i-2];
+        }
+    }  
+    return array;
+}
+
+void PrintArray(double[] array) {
+    for (int i = 0; i < array.Length; i++)
+    {   
+        if (i == array.Length - 1) {
+            Console.Write($"{array[i]}"); 
+        }
+        else {
+            Console.Write($"{array[i]}, "); 
+        }
+    }
+}
+
